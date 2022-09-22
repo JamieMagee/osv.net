5c1a8209630b679cdef95068e147296b16f4adec
Jamie Magee <jamie.magee@gmail.com>
2022-09-21 18:49:11 -0700

    fix: severity property is a list

--- src/OSV.Schema/Event.cs
+++ src/OSV.Schema/Event.cs
@@ -9,17 +9,17 @@ public sealed record Event
     /// The earliest version/commit where this vulnerability was introduced in.
     /// </summary>
     [JsonPropertyName("introduced")]
-    public string Introduced { get; set; } = null!;
+    public string? Introduced { get; set; } = null!;
 
     /// <summary>
     /// The version/commit that this vulnerability was fixed in.
     /// </summary>
     [JsonPropertyName("fixed")]
-    public string Fixed { get; set; } = null!;
+    public string? Fixed { get; set; } = null!;
 
     /// <summary>
     /// The limit to apply to the range.
     /// </summary>
     [JsonPropertyName("limit")]
-    public string Limit { get; set; } = null!;
+    public string? Limit { get; set; } = null!;
 }
