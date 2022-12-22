d658f88b17285de2cee32b28b46d7a4b7a6bf910
Jamie Magee <jamie.magee@gmail.com>
2022-12-21 22:58:00 -0800

    fix: update `SeverityType` enum
    `UNSPECIFIED` was removed, and `CVSS_V2` was added
    
    https://github.com/ossf/osv-schema/pull/87/files

--- src/OSV.Schema/SeverityType.cs
+++ src/OSV.Schema/SeverityType.cs
@@ -6,8 +6,8 @@
 [JsonConverter(typeof(JsonStringEnumMemberConverter))]
 public enum SeverityType
 {
-    [EnumMember(Value = "UNSPECIFIED")]
-    Unspecified,
+    [EnumMember(Value = "CVSS_V2")]
+    CvssV2,
     [EnumMember(Value = "CVSS_V3")]
     CvssV3,
 }
