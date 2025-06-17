6d925f82249848d00cf81f8d5487c9ad16750209
Jamie Magee <jamagee@microsoft.com>
2025-06-17 09:17:14 -0700

    Fix IDE0040

--- src/OSV.Client/IOSVClient.cs
+++ src/OSV.Client/IOSVClient.cs
@@ -2,9 +2,9 @@
 
 public interface IOSVClient
 {
-    Task<VulnerabilityList> QueryAffectedAsync(Query query, CancellationToken cancellationToken = default);
+    public Task<VulnerabilityList> QueryAffectedAsync(Query query, CancellationToken cancellationToken = default);
 
-    Task<BatchVulnerabilityList> QueryAffectedBatchAsync(BatchQuery batchQuery, CancellationToken cancellationToken = default);
+    public Task<BatchVulnerabilityList> QueryAffectedBatchAsync(BatchQuery batchQuery, CancellationToken cancellationToken = default);
 
-    Task<Vulnerability> GetVulnerabilityAsync(string id, CancellationToken cancellationToken = default);
+    public Task<Vulnerability> GetVulnerabilityAsync(string id, CancellationToken cancellationToken = default);
 }
